727c09f319446f4b5187ca48d09651693bd12b56
Benjamin Haeberli <admin@benjaminhaeberli.ch>
2022-09-17 16:24:53 +0200

    🐛 dump() redeclared by kirby

--- index.php
+++ index.php
@@ -2,17 +2,16 @@
 
 declare(strict_types=1);
 
+define('KIRBY_HELPER_DUMP', false);
 
 require __DIR__  . '/kirby/bootstrap.php';
 
-/* use DebugBar\StandardDebugBar;
-
-$debugbar = new StandardDebugBar();
+$debugbar = new DebugBar\StandardDebugBar();
 $debugbar['time']->startMeasure('kirbycms', 'Kirby CMS');
-$debugbarRenderer = $debugbar->getJavascriptRenderer(); */
+$debugbarRenderer = $debugbar->getJavascriptRenderer();
 
 echo (new Kirby\Cms\App)->render();
 
-/* $debugbar['time']->stopMeasure('kirbycms'); */
-/* echo $debugbarRenderer->renderHead();
-echo $debugbarRenderer->render(); */
+$debugbar['time']->stopMeasure('kirbycms');
+echo $debugbarRenderer->renderHead();
+echo $debugbarRenderer->render();
