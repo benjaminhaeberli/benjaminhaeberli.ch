27fbc48324ecd18d36acbd66acd05be372f8c780
Benjamin Haeberli <admin@benjaminhaeberli.ch>
2023-05-13 01:22:36 +0200

    ➕ rector config

--- rector.php
+++ rector.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\SetList;
 use Rector\Set\ValueObject\LevelSetList;
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
@@ -17,17 +16,13 @@ return static function (RectorConfig $rectorConfig): void {
         __DIR__ . '/tests',
     ]);
 
-    // Register a single rule
-    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
-
-    // Define sets of rules
+    #  SetList::PRIVATIZATION
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_81,
+        SetList::PHP_81,
         SetList::CODE_QUALITY,
         SetList::DEAD_CODE,
-        SetList::EARLY_RETURN,
         SetList::TYPE_DECLARATION,
-        SetList::PRIVATIZATION,
     ]);
 
     $rectorConfig->autoloadPaths([
