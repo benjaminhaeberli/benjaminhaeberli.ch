645d9a1b746fc0542fa91b0df4e7ef2858011e4e
Benjamin Haeberli <admin@benjaminhaeberli.ch>
2023-05-19 18:07:50 +0200

    💬 content (hide for prod)

---
+++ site/templates/error.php
@@ -0,0 +1,20 @@
+ss
+<?php snippet('header') ?>
+
+<main class="flex flex-col gap-16">
+    <section class="container max-w-screen-lg mx-auto">
+        <div class="flex flex-col max-w-2xl gap-4 text-slate-900">
+            <p class="font-mono font-bold uppercase text-slate-600">Qu'est-ce que tu fais là ? 🤔</p>
+            <h1 class="text-4xl font-bold">
+                Erreur !
+            </h1>
+            <p>Je suis désolé de t'annoncer que cette page n'existe pas. Peut-être un jour, qui sait ?</p>
+            <div class="flex gap-4">
+                <a href="/" class="bh__btn">Retour à l'accueil</a>
+            </div>
+        </div>
+    </section>
+</main>
+
+
+<?php snippet('footer') ?>
