b58745731eb7f027504e0938c87239f42f4342b2
Benjamin Haeberli <admin@benjaminhaeberli.ch>
2023-06-11 20:54:30 +0200

    💬 content

--- site/snippets/megamenu.php
+++ site/snippets/megamenu.php
@@ -5,7 +5,7 @@ use BenjaminHaeberli\Portfolio\KirbyHelper;
 ?>
 
 <?php if (KirbyHelper::siteHasField('megamenu')) : ?>
-    <nav class="container flex max-w-screen-xl gap-2 pb-4 mx-auto text-lg font-semibold">
+    <nav class="flex gap-2 pb-4 mx-auto text-lg font-semibold">
         <ol class="flex gap-2">
             <?php
             $links = site()->megamenu()->toStructure();
@@ -14,9 +14,9 @@ use BenjaminHaeberli\Portfolio\KirbyHelper;
                 <?php if (($page = KirbyHelper::isPage($page)) !== null) : ?>
                     <li class="flex">
                         <?php if (page()->is($page)) : ?>
-                            <a href="#" class="flex items-center gap-2 px-2 py-2 group focus:bg-transparent text-zinc-950 bg-primary hover:bg-slate-800 focus:text-slate-200 focus:outline-dashed outline-2 ">
+                            <a href="#" class="flex items-center gap-2 px-2 py-2 group focus:bg-transparent text-zinc-950 bg-primary hover:bg-secondary focus:text-slate-200 focus:outline-dashed outline-2 ">
                             <?php else : ?>
-                                <a href="<?= $page->url() ?>" class="flex items-center gap-2 px-2 py-2 group focus:bg-transparent hocus:text-slate-800 focus:outline-dashed outline-2">
+                                <a href="<?= $page->url() ?>" class="flex items-center gap-2 px-2 py-2 group focus:bg-transparent hocus:text-primary focus:outline-dashed outline-2">
                                 <?php endif; ?>
                                 <?= $link->svg_icon() ?>
                                 <span> <?= $page->title() ?></span>
