59ed8d9c703c829491c4c5972014407578142a31
Benjamin Haeberli <admin@benjaminhaeberli.ch>
2023-11-07 19:34:11 +0100

    🩹 remove .env (not used)

--- index.php
+++ index.php
@@ -6,13 +6,6 @@
 require __DIR__  . '/kirby/bootstrap.php';
 
 /**
- * Loads environment variables from `.env` file
- * @see https://github.com/vlucas/phpdotenv
- */
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-/**
  * Launch Kirby
  */
 $kirby = new Kirby\Cms\App();
