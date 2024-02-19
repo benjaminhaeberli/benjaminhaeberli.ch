39857d0211ec28c90a532babc86ddff1bf5ad0f9
Benjamin Haeberli <admin@benjaminhaeberli.ch>
2024-02-19 11:09:23 +0100

    🩹 pass tests

--- index.php
+++ index.php
@@ -2,19 +2,6 @@
 <?php
 
 /**
- * They don't use `functions_exists()` knowingly, to simplify the use of the CMS. The problem is
- * that when using other libraries like PHPUnit or Ray, Kirby's code is not necessarily loaded
- * first and this creates "Cannot redeclare" PHP errors. It is therefore mandatory to define the
- * constants in a file that is loaded first,like this one.
- *
- * @see https://github.com/getkirby/kirby/issues/4462
- * @see https://getkirby.com/docs/guide/troubleshooting/installation#php-cannot-redeclare-error
- * @see https://getkirby.com/docs/reference/objects/cms/helpers/has-override
- * @see https://getkirby.com/docs/reference/templates/helpers
- */
-define('KIRBY_HELPER_DUMP', false);
-
-/**
  * Kirby Autoloader
  */
 require_once __DIR__  . '/kirby/bootstrap.php';
