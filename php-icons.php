62dd1450b3ac1766af8dd718329a0eff6666cb1e
Benjamin Haeberli <admin@benjaminhaeberli.ch>
2024-09-23 01:37:41 +0200

    💚 fix ci

--- php-icons.php
+++ php-icons.php
@@ -6,8 +6,8 @@ use PHPIcons\Config\PHPIconsConfig;
 
 return PHPIconsConfig::configure()
     ->withPaths([
-        __DIR__.'\resources',
-        __DIR__.'\src',
+        __DIR__.DIRECTORY_SEPARATOR.'resources',
+        __DIR__.DIRECTORY_SEPARATOR.'src',
     ])
     ->withDefaultPrefix('')
     ->withIdentifiers(['phpicon', 'icon'])
