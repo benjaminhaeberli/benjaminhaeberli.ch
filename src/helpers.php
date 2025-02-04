010604bb45d29c8366ea8e94b72d66953a538b53
benjaminhaeberli <33510361+benjaminhaeberli@users.noreply.github.com>
2025-02-04 08:33:30 +0000

    🎨 pint

--- src/helpers.php
+++ src/helpers.php
@@ -6,8 +6,8 @@ if (! function_exists('formatDate')) {
     function formatDate(string $dateString): void
     {
         $locale = 'fr_CH';
-        $dateType = IntlDateFormatter::LONG; //type of date formatting
-        $timeType = IntlDateFormatter::NONE; //type of time formatting setting to none, will give you date itself
+        $dateType = IntlDateFormatter::LONG; // type of date formatting
+        $timeType = IntlDateFormatter::NONE; // type of time formatting setting to none, will give you date itself
         $formatter = new IntlDateFormatter($locale, $dateType, $timeType);
         $dateTime = new DateTime($dateString);
         echo $formatter->format($dateTime);
